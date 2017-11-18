ec77ea475ae4d12877a695fbfac192d6962f72a5
lepetinez <lepetinez@gmail.com>
2017-11-18 17:49:46 +0100

    event and tamplate controllers created

---
+++ src/main/java/sat/controller/TemplateController.java
@@ -0,0 +1,10 @@
+package sat.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("event")
+public class TemplateController {
+
+}
